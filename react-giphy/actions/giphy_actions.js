eb14dda97b460b5dae75aa74e2aa6bef05802606
Ryan <alejandr257@gmail.com>
2018-10-14 21:51:23 -0700

    create giphy actions, giphy reducer, root reducer

--- react-giphy/actions/giphy_actions.js
+++ react-giphy/actions/giphy_actions.js
@@ -1,1 +1,18 @@
 import * as APIUtil from '../util/api_util';
+
+export const RECEIVE_SEARCH_GIPHYS = 'RECEIVE_SEARCH_GIPHYS';
+export const RECEIVE_TRENDING_GIPHYS = 'RECEIVE_TRENDING_GIPHYS';
+
+ export const receiveSearchGiphys = giphys => {
+   return {
+     type: RECEIVE_SEARCH_GIPHYS,
+     giphys
+   }
+ };
+
+export const receiveTrendingGiphys = giphys => {
+  return {
+    type: RECEIVE_TRENDING_GIPHYS,
+    giphys
+  }
+}
